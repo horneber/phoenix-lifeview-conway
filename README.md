246d173d7cb175ccbda59d47cb690586a9378589
Henrik Horneber <henrik.horneber@conti.de>
2019-05-12 21:32:32 +0200

    more keyboard bindings, some explanation

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Naive implementation of game of life on a 100x100 grid.
 Playing around with Phoenix LiveView.
+
+May or may not be running at https://intense-oasis-66457.herokuapp.com/
--- lib/life_web/live/grid_view.ex
+++ lib/life_web/live/grid_view.ex
@@ -7,7 +7,7 @@ defmodule LifeWeb.GridView do
   def render(assigns) do
     board_size = @board_size
     ~L"""
-    <table  >
+    <table  phx-keyup="window_key_event" phx-target="window">
       <%= for y <- board_size..1 do %>
         <tr>
           <%= for x <- 1..board_size do %>
@@ -16,22 +16,27 @@ defmodule LifeWeb.GridView do
         </tr>
       <% end %>
     </table>
-    <div class="">
-      <div>
-        <div>
-          <button phx-click="next_gen">Next Generation</button>
-          <button phx-click="start_auto">Start auto</button>
-          <button phx-click="stop_auto">Stop auto</button>
-        </div>
-        Status: <%= @generation %>
-      </div>
+    <div >
+      <%= if @tref  do %>
+        <button phx-click="stop_auto">Stop auto</button>
+      <% else %>
+        <button phx-click="next_gen">Next Generation</button>
+        <button phx-click="start_auto">Start auto</button>
+      <% end %>
+    </div>
+    <div>
+      Generation: <%= @generation %>
+    </div>
+    <div>
+        Arrow keys move the grid around. Keys <code>w s a d</code> scroll a little faster. <br>
+        Cells are click-able. <code>Space</code> starts and stops the automaton.
     </div>
     """
   end
 
   def mount(_session, socket) do
     Logger.debug "Mounting!"
-    {:ok, assign(socket, generation: 0, grid: Life.Grids.interesting_starter())}
+    {:ok, assign(socket, generation: 0, grid: Life.Grids.interesting_starter(), tref: nil)}
   end
 
   def handle_event("toggle", value,  %{assigns: %{ grid: grid}} = socket) do
@@ -41,43 +46,69 @@ defmodule LifeWeb.GridView do
     {:noreply, assign(socket, grid: grid)}
   end
 
-  def handle_event("next_gen", _value, socket) do
-    handle_info(:next_gen, socket)
-  end
 
-  def handle_event("start_auto", _value,  %{assigns: %{ tref: tref}} = socket) when is_nil(tref) do
+  def handle_event("start_auto", _value,  %{assigns: %{ tref: nil}} = socket)  do
     start_auto(socket)
   end
 
   def handle_event("start_auto", _value,  %{assigns: %{ tref: _tref,}} = socket) do
-    Logger.debug "Start_auto with tref"
     {:noreply, socket}
   end
 
-  def handle_event("start_auto", _value,  socket) do
+  def handle_event("stop_auto", _value,  %{assigns: %{ tref: nil}} = socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("stop_auto", _value,  %{assigns: %{ tref: tref}} = socket) do
+    stop_auto(socket, tref)
+  end
+
+  def handle_event("next_gen", _value, socket) do
+    handle_info(:next_gen, socket)
+  end
+
+  def handle_event("window_key_event", " ", %{assigns: %{ tref: nil }} = socket) do
     start_auto(socket)
   end
 
-  def start_auto(socket) do
-    {:ok, tref} = :timer.send_interval(500, self(), :next_gen)
-    {:noreply, assign(socket, tref: tref)}
+  def handle_event("window_key_event", " ", %{assigns: %{ tref: tref }} = socket) do
+    stop_auto(socket, tref)
   end
 
-
-
-  def handle_event("stop_auto", _value,  %{assigns: %{ tref: tref}} = socket) do
-    :timer.cancel(tref)
-#    socket = update_in(socket.assigns, &Map.drop(&1, [:tref]))
-    {:noreply, update(socket, :tref, fn _ -> nil end)}
+  def handle_event("window_key_event", "ArrowUp", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 1}))}
   end
 
-  def handle_event("stop_auto", _value,  %{assigns: %{ tref: tref}} = socket) when is_nil(tref) do
-    Logger.debug "Stop_auto without tref and nil"
-    {:noreply, socket}
+  def handle_event("window_key_event", "ArrowDown", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -1}))}
   end
 
-  def handle_event("stop_auto", _value,  socket) do
-    Logger.debug "Stop_auto without tref"
+  def handle_event("window_key_event", "ArrowLeft", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {-1, 0}))}
+  end
+
+  def handle_event("window_key_event", "ArrowRight", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {1, 0}))}
+  end
+
+  def handle_event("window_key_event", "w", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 4}))}
+  end
+
+  def handle_event("window_key_event", "s", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -4}))}
+  end
+
+  def handle_event("window_key_event", "a", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {-4, 0}))}
+  end
+
+  def handle_event("window_key_event", "d", %{assigns: %{ grid: grid }} = socket) do
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {4, 0}))}
+  end
+
+  def handle_event("window_key_event", any_other_key, %{assigns: %{ grid: grid }} = socket) do
+    Logger.debug "unhandled key: #{inspect(any_other_key)}"
     {:noreply, socket}
   end
 
@@ -88,4 +119,13 @@ defmodule LifeWeb.GridView do
     {:noreply, assign(socket, generation: generation + 1, grid: next_grid)}
   end
 
+  def start_auto(socket) do
+    {:ok, tref} = :timer.send_interval(500, self(), :next_gen)
+    {:noreply, assign(socket, tref: tref)}
+  end
+
+  def stop_auto(socket, tref) do
+    :timer.cancel(tref)
+    {:noreply, assign(socket, tref: nil)}
+  end
 end
