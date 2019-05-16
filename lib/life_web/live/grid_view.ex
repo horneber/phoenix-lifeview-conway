0396c4e7a2b1f7dc4e019749f7ec29a36b0f9163
Henrik Horneber <henrik.horneber@conti.de>
2019-05-16 21:59:04 +0200

    moar svg, edit mode

--- lib/life_web/live/grid_view.ex
+++ lib/life_web/live/grid_view.ex
@@ -8,17 +8,21 @@ defmodule LifeWeb.GridView do
     LifeWeb.PageView.render("grid_view.html", assigns)
   end
 
-  @default_timer_interval 333
-  @default_grid_size 60
+  @default_timer_interval 250
+  @default_grid_size 120
   def mount(_session, socket) do
     Logger.debug "Mounting!"
+    grid = Life.Grids.interesting_starter()
     if connected?(socket), do: Logger.debug "Connected."
     {:ok, assign(socket,
       generation: 0,
-      grid: Life.Grids.interesting_starter(),
+      grid: grid,
       tref: nil,
       grid_size: @default_grid_size,
-      timer_interval: @default_timer_interval)}
+      timer_interval: @default_timer_interval,
+      edit?: false,
+      largest_population_ever: MapSet.size(grid)
+    )}
   end
 
   def handle_event("toggle", value,  %{assigns: %{ grid: grid}} = socket) do
@@ -28,6 +32,13 @@ defmodule LifeWeb.GridView do
     {:noreply, assign(socket, grid: grid)}
   end
 
+  def handle_event("stop_editing", _value,   socket)  do
+    {:noreply, assign(socket, edit?: false)}
+  end
+
+  def handle_event("start_editing", _value,   socket)  do
+    {:noreply, assign(socket, edit?: true)}
+  end
 
   def handle_event("start_auto", _value,  %{assigns: %{ tref: nil}} = socket)  do
     start_auto(socket)
@@ -54,19 +65,28 @@ defmodule LifeWeb.GridView do
     {:noreply, socket}
   end
 
+  @minimum_interval 100
   def handle_event("save", %{"controls" => controls}, socket) do
     Logger.debug "save #{inspect(controls)}"
-    %{"grid_size" => grid_size } = controls
-    %{"timer_interval" => timer_interval } = controls
-    grid_size = case Integer.parse(grid_size) do
+    grid_size = case Integer.parse(controls["grid_size"]) do
       {grid_size, _} -> grid_size
       :error -> @default_grid_size
     end
-    timer_interval = case Integer.parse(timer_interval) do
-      {timer_interval, _} -> timer_interval
+    timer_interval = case Integer.parse(controls["timer_interval"]) do
+      {timer_interval, _} -> max(timer_interval, @minimum_interval)
       :error -> @default_timer_interval
     end
-    {:noreply, assign(socket, timer_interval: timer_interval, grid_size: grid_size)}
+    Logger.debug("new timer #{inspect(timer_interval)}}")
+    tref = socket.assigns.tref
+    socket = if tref do
+      Logger.debug("On the fly new timer.")
+      :timer.cancel(tref)
+      tref = new_timer(timer_interval)
+      assign(socket, timer_interval: timer_interval, grid_size: grid_size, tref: tref)
+    else
+      assign(socket, timer_interval: timer_interval, grid_size: grid_size)
+    end
+    {:noreply, socket}
   end
 
   def handle_event("window_key_event", " ", %{assigns: %{ tref: nil }} = socket) do
@@ -78,11 +98,11 @@ defmodule LifeWeb.GridView do
   end
 
   def handle_event("window_key_event", "ArrowUp", %{assigns: %{ grid: grid }} = socket) do
-    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 1}))}
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -1}))}
   end
 
   def handle_event("window_key_event", "ArrowDown", %{assigns: %{ grid: grid }} = socket) do
-    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -1}))}
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 1}))}
   end
 
   def handle_event("window_key_event", "ArrowLeft", %{assigns: %{ grid: grid }} = socket) do
@@ -94,11 +114,11 @@ defmodule LifeWeb.GridView do
   end
 
   def handle_event("window_key_event", "w", %{assigns: %{ grid: grid }} = socket) do
-    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 4}))}
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -4}))}
   end
 
   def handle_event("window_key_event", "s", %{assigns: %{ grid: grid }} = socket) do
-    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, -4}))}
+    {:noreply, assign(socket, :grid, Life.Grids.transpose(grid, {0, 4}))}
   end
 
   def handle_event("window_key_event", "a", %{assigns: %{ grid: grid }} = socket) do
@@ -114,20 +134,36 @@ defmodule LifeWeb.GridView do
     {:noreply, socket}
   end
 
-  def handle_info(:next_gen, %{assigns: %{ grid: grid, generation: generation }} = socket) do
+  def handle_info(:next_gen,
+        %{assigns:
+           %{ grid: grid, generation: generation, largest_population_ever: largest_population_ever }
+        } = socket) do
     # Logger.debug "next_gen for #{inspect(grid)}}"
     next_grid = Grid.calc_next_grid(grid)
+    largest_population_ever = max(largest_population_ever, MapSet.size(next_grid))
     # Logger.debug "next_gen is #{inspect(next_grid)}}"
-    {:noreply, assign(socket, generation: generation + 1, grid: next_grid)}
+    {:noreply,
+      assign(socket,
+        generation: generation + 1,
+        grid: next_grid,
+        largest_population_ever: largest_population_ever
+    )}
   end
 
   def start_auto(socket) do
     %{assigns: %{timer_interval: timer_interval}} = socket
-    {:ok, tref} = :timer.send_interval(timer_interval,  self(), :next_gen)
+    tref = new_timer(timer_interval)
     {:noreply, assign(socket, tref: tref)}
   end
 
+  def new_timer(timer_interval) do
+    Logger.debug "new timer"
+    {:ok, tref} = :timer.send_interval(timer_interval,  self(), :next_gen)
+    tref
+  end
+
   def stop_auto(socket, tref) do
+    Logger.debug "stop_auto"
     :timer.cancel(tref)
     {:noreply, assign(socket, tref: nil)}
   end
